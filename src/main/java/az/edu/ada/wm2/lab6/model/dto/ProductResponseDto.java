1655a8725dc88fddda2d55b4627632615db34e17
Farid Aslanli <119242478+Farid181@users.noreply.github.com>
2026-04-02 15:28:06 +0400

    Implement ProductService with full logic and passing tests

---
+++ src/main/java/az/edu/ada/wm2/lab6/model/dto/ProductResponseDto.java
@@ -0,0 +1,23 @@
+package az.edu.ada.wm2.lab6.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponseDto {
+    private UUID id;
+    private String productName;
+    private BigDecimal price;
+    private LocalDate expirationDate;
+    private LinkedList<String> categoryNames;
+}
